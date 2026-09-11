7476fa5bd946e1e6129d8a9bcc894f7f7ac19249
shubhash kumar <subhashsharmandhruv@gmail.com>
2026-09-11 20:42:51 +0530

    seperate module for file read and write

---
+++ reading and writing from a file/module.js
@@ -0,0 +1,51 @@
+const fs=require("fs");
+const requestHandler=(req, res) =>
+{ 
+  if (req.url === "/" && req.method==="GET")
+   {
+    let userDetails = "";
+     fs.readFile("userDetails.txt", "utf8", (err, data) => 
+     {
+      
+      if (!err && data) {
+       userDetails = `${data}`
+      }
+    res.setHeader("Content-Type", "text/html");
+    return res.end(`
+        ${userDetails}
+        <form action="/submit" method="POST">
+        <label for="username">username:</label>
+        <input type="text" id="username" name="username">
+        <button type="submit">Send</button>
+    </form>`);
+      });
+    }
+  else if (req.url === "/submit" && req.method === "POST") 
+    {
+    let body = [];
+    req.on("data", (chunks) => {
+      body.push(chunks);
+      console.log(chunks);
+    })
+    req.on("end",()=>
+    {
+        const bodyString=Buffer.concat(body).toString();
+        console.log(bodyString);
+        const formDataObj={};
+        const formData= new URLSearchParams(bodyString);
+        console.log(formData);
+         for(const [key,value] of formData)
+         {
+            formDataObj[key]=value;
+         }
+          fs.writeFile('userDetails.txt', bodyString,(error)=>
+        {
+            res.writeHead(302,{'Location':'/'});
+            res.end();   
+        });
+         
+    })
+  }
+}
+
+module.exports=requestHandler;
