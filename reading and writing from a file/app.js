a66fd8b074550fa81d3c5d216237cc3f0cfc8877
shubhash kumar <subhashsharmandhruv@gmail.com>
2026-09-11 14:16:51 +0530

    reading and writing from file

---
+++ reading and writing from a file/app.js
@@ -0,0 +1,43 @@
+const http = require("http");
+const fs=require('fs');
+const { error } = require("console");
+const PORT = 3000;
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    return res.end(`<form action="/submit" method="POST">
+        <label for="username">username:</label>
+        <input type="text" id="username" name="username">
+        <button type="submit">Send</button>
+    </form>`);
+  }
+ else if (req.url === "/submit" && req.method === "POST") 
+    {
+    let body = [];
+    req.on("data", (chunks) => {
+      body.push(chunks);
+      console.log(chunks);
+    })
+    req.on("end",()=>
+    {
+        const bodyString=Buffer.concat(body).toString();
+        console.log(bodyString);
+        const formDataObj={};
+        const formData= new URLSearchParams(bodyString);
+        console.log(formData);
+         for(const [key,value] of formData)
+         {
+            formDataObj[key]=value;
+         }
+          fs.writeFile('userDetails.txt', bodyString,(error)=>
+        {
+            res.writeHead(302,{'Location':'/'});
+            return res.end("Details saved!");
+        });
+         
+    })
+  }
+});
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
